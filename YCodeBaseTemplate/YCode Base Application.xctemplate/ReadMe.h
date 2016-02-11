cfd0bc384af521a7714cc8a5bedf3b75892c3163
顾强 <guqiang@ycode.cn>
2016-02-11 23:37:52 +0800

    init

---
+++ YCodeBaseTemplate/YCode Base Application.xctemplate/ReadMe.h
@@ -0,0 +1,13 @@
+//
+//  ReadMe.h
+//  DemoTest
+//
+//  Created by YCode on 16/2/6.
+//  Copyright © 2016年 YCode. All rights reserved.
+//
+
+#ifndef ReadMe_h
+#define ReadMe_h
+
+
+#endif /* ReadMe_h */
