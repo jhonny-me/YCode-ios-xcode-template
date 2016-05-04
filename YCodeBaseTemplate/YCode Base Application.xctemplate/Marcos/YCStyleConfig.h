3483141cfb1fcaecc74f7b2dbd59dc5f458853d5
顾强 <guqiang@ycode.cn>
2016-05-04 16:44:26 +0800

    styleConfig

---
+++ YCodeBaseTemplate/YCode Base Application.xctemplate/Marcos/YCStyleConfig.h
@@ -0,0 +1,92 @@
+//
+//  YCStyleConfig.h
+//  ZeroBuy
+//
+//  Created by 顾强 on 16/5/4.
+//  Copyright © 2016年 namei. All rights reserved.
+//
+
+#ifndef YCStyleConfig_h
+#define YCStyleConfig_h
+
+/**************normal**********/
+#define STYLE_MAINCOLOR  @"#FF6D42"
+
+
+/*******导航栏颜色************/
+#define SYTLE_NAVIGATION_CLR @"#FF6D42"
+
+#define SUB_NAVIGATION_CLR @"#F2F2F2"
+
+
+
+
+/**********字体规范***********/
+
+
+
+//顶部导航
+#define STYLE_FONT_TOPNAVI  18
+//按钮文字
+#define STYLE_FONT_BUTTON   15
+//顶部标签栏，正文，商品描述
+#define STYLE_FONT_CONTENT  13
+//说明文字、提示文字
+#define STYLE_FONT_TIPS     12
+//菜单栏文字
+#define STYLE_FONT_MENU     11
+
+
+
+/**********按钮规范***********/
+
+//默认颜色
+#define STYLE_BTN_CLR_DEFULT    STYLE_MAINCOLOR
+//不可用颜色
+#define STYLE_BTN_CLR_UNABLE    @"#E2E2E2"
+//按下颜色
+#define STYLE_BTN_CLR_TOUCHDOWN @"#EB4E20"
+//按钮文字颜色
+#define STYLE_BTN_CLR_TXT       @"#FFFFFF"
+//按钮边框颜色
+#define STYLE_BTN_CLR_BOUNDS    @"#FF6D42"
+//按钮弧形边框颜色
+#define STYLE_BTN_BOLD           @"#333333"
+//按钮新配色
+#define STYLE_BTN_BLCK           @"#515151"
+//按钮边框颜色(灰色)
+#define STYLE_BTN_GRAY          @"#D2D2D2"
+//按钮圆边
+#define STYLE_BTN_RADUIS   5.0
+
+
+/********输入框***************/
+#define STYLE_TXTFD_CLR_BOUNDS @"#D2D2D2"
+
+
+
+
+/*********文字颜色*********/
+
+//文字默认色
+#define STYLE_WORDCLR_DEFAULT  @"#515151"
+//文字灰色
+#define STYLE_WORDCLR_GRAY     @"#999999"
+//标题文字
+#define STYLE_WORDCLR_GREY     @"#cccccc"
+//非纯黑色
+#define STYLE_WORDCLR_BLACK    @"#333333"
+//文字绿色
+#define STYLE_WORDCLR_GREEN    @"#28ac60"
+/********背景色********/
+
+//主背景色
+#define STYLE_BGNDCLR_MAIN @"#F3F3F3"
+//背景淡灰
+#define STYLE_BGNDCLR_LIGHTGRAY @"#DBDBDB"
+#define STYLE_BGDCLR_GRAY       @"#E8E8E8"
+#define STYLE_CELL_BLACK        @"#434a53"
+#define STYLE_TEVTVIEW_BLACK    @"#D1D1D5"
+#define STYLE_WHITE             @"#FFFFFF"
+
+#endif /* YCStyleConfig_h */
